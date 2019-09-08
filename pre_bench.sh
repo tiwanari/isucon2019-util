04f93d79c9e9bfbd370ffa50ff9087263f6c701d
Isucon9 Root <you@example.com>
2019-09-08 02:07:54 +0000

    Rotate sql slow query log in pre_bench

--- pre_bench.sh
+++ pre_bench.sh
@@ -7,12 +7,18 @@ echo "making $LOG_DIR ..."
 mkdir -p $LOG_DIR
 
 ACCESS_LOG=/var/log/nginx/access.log
+SLOW_QUERY_LOG=/var/log/mysql/slow.log
 SUFFIX=`date +%Y%m%d-%H%M%S`
 
 ## Rotate access logs
 echo "copying and initializing $ACCESS_LOG ..."
 cp $ACCESS_LOG $LOG_DIR/access_$SUFFIX.log
 cat /dev/null > $ACCESS_LOG 
+
+## Rotate access logs
+echo "copying and initializing $SLOW_QUERY_LOG ..."
+cp $SLOW_QUERY_LOG $LOG_DIR/slow_$SUFFIX.log
+cat /dev/null > $SLOW_QUERY_LOG
 
 ## Recompile the service
 echo "making go webapp ..."
